60e229dc663a84cde389d7618536c1b82d31d3aa
Mrinal Chandra <dmrinal626@gmail.com>
2019-02-08 11:17:23 +0530

    maven testing

--- Calculator/src/main/java/main/Calculator.java
+++ Calculator/src/main/java/main/Calculator.java
@@ -1,4 +1,5 @@
 package main;
+
 import java.util.*; 
 
 public class Calculator {
--- Calculator/src/main/java/test/TestingCalc.java
+++ Calculator/src/main/java/test/TestingCalc.java
@@ -1,8 +1,8 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import main.Calculator;
 
